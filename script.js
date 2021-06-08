3d0bc6a8ac312a48f80e897a0a51bb3a7c92c9e6
Tyson Wellings <tyson.s3510@gmail.com>
2021-06-08 17:01:23 +1000

    Started playing around with a slider that controls the x-axis range. Hopefully I can figure out how to animate it.

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ dataSource1 = "Trump_Tweets_Frequency.csv"
 function makePlot1(data) {
 
     function unpack(data, key) {
-        return data.map(function(data) { return data[key]; });
-      }
+        return data.map(function (data) { return data[key]; });
+    }
 
     var data = [{
         type: "bar",
         name: 'Trump Tweets',
         x: unpack(data, 'date'),
         y: unpack(data, 'Frequency'),
-        line: {color: 'black'}
+        line: { color: 'black' }
     }]
 
     console.log(data)
-   
+
     var layout = { //layout information for the graph pretty standard 
 
         width: 800,
@@ -26,7 +26,7 @@ function makePlot1(data) {
             color: "black"
         },
         xaxis: {
-            rangeslider: {}
+            
         },
         yaxis: {
             fixedrange: true
@@ -35,33 +35,63 @@ function makePlot1(data) {
             width: 1
         },
         hovermode: 'closest',
+
+        sliders: [{
+            pad: { t: 30 },
+            currentvalue: {
+                xanchor: 'right',
+                prefix: 'Year: ',
+                font: {
+                    color: '#888',
+                    size: 20
+                }
+            },
+            steps: [{
+                label: '2017',
+                method: 'restyle',
+                args: ['x-axis.range', '[1-1-2017,31-12-2017']
+            }, {
+                label: '2018',
+                method: 'restyle',
+                args: ['x-axis.range', '[1-1-2018,31-12-2018']
+            }, {
+                label: '2019',
+                method: 'restyle',
+                args: ['x-axis.range', '[1-1-2019,31-12-2019']
+            }, {
+                label: '2020',
+                method: 'restyle',
+                args: ['x-axis.range', '[1-1-2020,8-1-2021']
+            }]
+        }]
     };
 
 
-    
-     // Create the plot:
+
+    // Create the plot:
     Plotly.newPlot('firstPlot', {
         data: data,
         layout: layout,
         config: {
-            displayModeBar: false} 
-    }); 
+            displayModeBar: false
+        }
+    });
 }
 
 //from https://codereview.stackexchange.com/a/171857
-function convertToParagraph(sentence, maxLineLength){
+function convertToParagraph(sentence, maxLineLength) {
     let lineLength = 0;
     sentence = sentence.split(" ")
     return sentence.reduce((result, word) => {
-      if (lineLength + word.length >= maxLineLength) {
-        lineLength = word.length;
-        return result + `<br>${word}`;
-      } else {
-        lineLength += word.length + (result ? 1 : 0);
-        return result ? result + ` ${word}` : `${word}`;
-      }
+        if (lineLength + word.length >= maxLineLength) {
+            lineLength = word.length;
+            return result + `<br>${word}`;
+        } else {
+            lineLength += word.length + (result ? 1 : 0);
+            return result ? result + ` ${word}` : `${word}`;
+        }
     }, '');
-  }
+}
 
 function make_plot(tweet_data, tsne_data) {
     let data = [{
@@ -79,7 +109,7 @@ function make_plot(tweet_data, tsne_data) {
             color: tsne_data.map(d => d.cluster_id),
         }
     }];
-  
+
     let layout = {
         width: 800,
         hovermode: "closest",
@@ -90,14 +120,14 @@ function make_plot(tweet_data, tsne_data) {
             visible: false,
         }
     }
-  
+
     Plotly.newPlot('secondPlot', data, layout);
-  }
-  
+}
+
 
 Plotly.d3.csv(dataSource1, function (data) { makePlot1(data) })
 Plotly.d3.csv("trump_presidential_tweets.csv", (tweets) => {
     Plotly.d3.csv("tsne_trump_data_2020_2021.csv", (tnse_data) => {
-      make_plot(tweets, tnse_data)
+        make_plot(tweets, tnse_data)
     });
-  });
+});
