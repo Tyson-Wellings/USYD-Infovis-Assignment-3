8ea5d6b8bf94f8145107d225267b519e07b8f990
Tyson Wellings <tyson.s3510@gmail.com>
2021-06-11 18:58:55 +1000

    Working on the third graph

--- tsner.py
+++ tsner.py
@@ -2,7 +2,7 @@ import csv
 from sklearn.manifold import TSNE
 from sklearn.cluster import KMeans
 
-with open('trump_2019_vectors.csv') as vector_data:
+with open('blue_vectors.csv') as vector_data:
     vector_data_reader = csv.reader(vector_data)
     next(vector_data_reader, None) #skip headers
 
@@ -14,10 +14,11 @@ with open('trump_2019_vectors.csv') as vector_data:
     x_output = [float(row[0]) for row in embedded]
     y_output = [float(row[1]) for row in embedded]
 
-    clusters = KMeans(n_clusters=9).fit_predict(training_data) #run kmeans
+    clusters = KMeans(random_state=1 ,n_clusters=20,n_init=20).fit_predict(training_data) #run kmeans
 
 
-    with open('tsne_trump_2019.csv', mode='w', newline="") as output_file:
+    with open('tsne_blue.csv', mode='w', newline="") as output_file:
         output_writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         output_writer.writerow(["id", "x", "y", "cluster_id"]) #write headers
         output_writer.writerows(zip(tweet_ids, x_output, y_output, clusters)) #combine data
+
