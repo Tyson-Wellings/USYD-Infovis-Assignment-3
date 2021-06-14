91eb16a1961775836256c2c91707fbeb9ab3d698
Tyson Wellings <tyson.s3510@gmail.com>
2021-06-14 19:28:27 +1000

    Finished user testing the graphs. adjustements needed.

--- script.js
+++ script.js
@@ -23,22 +23,22 @@ function convertToParagraph(sentence, maxLineLength) {
     }, '');
 }
 
-function make_clusters_by_year(data){
-    
+function make_clusters_by_year(data) {
+
     clusters = {
         2017: {},
         2018: {},
         2019: {},
         2020: {}
-        }
+    }
 
-    
+
     var search_year = 2016;
     var visibility = true
     for (i = 0; i < data.length; i++) {
         var datum = data[i]
-        
-        if (parseInt(datum.year) !== search_year){
+
+        if (parseInt(datum.year) !== search_year) {
             clusters[datum.year] = {
                 x: [],
                 y: [],
@@ -68,15 +68,15 @@ function make_clusters_by_year(data){
     return clusters
 }
 
-function make_clusters_by_word(data){
-    
+function make_clusters_by_word(data) {
+
     clusters = []
     var search_word = -1;
     var visibility = true
     for (i = 0; i < data.length; i++) {
         var datum = data[i]
-        
-        if (parseInt(datum.word) !== search_word){
+
+        if (parseInt(datum.word) !== search_word) {
             clusters[datum.word] = {
                 x: [],
                 y: [],
@@ -128,7 +128,7 @@ function makePlot1(data) {
             color: "black"
         },
         xaxis: {
-            
+
         },
         yaxis: {
             fixedrange: true
@@ -191,47 +191,49 @@ function makePlot2(data) {
 
     clusters = make_clusters_by_year(data)
 
-    var data = [clusters[2017],clusters[2018],clusters[2019],clusters[2020]
+    var data = [clusters[2017], clusters[2018], clusters[2019], clusters[2020]
     ];
 
-    var updatemenus=[
+    console.log(data)
+
+    var updatemenus = [
         {
             buttons: [
                 {
-                    args: [{'visible': [true, false, false, false]},
-                           {'title': '2017 cluster'}],
+                    args: [{ 'visible': [true, false, false, false] },
+                    { 'title': '2017 cluster' }],
                     label: '2017',
                     method: 'update'
                 },
                 {
-                    args: [{'visible': [false, true, false, false,]},
-                           {'title': '2018 cluster'}],
+                    args: [{ 'visible': [false, true, false, false,] },
+                    { 'title': '2018 cluster' }],
                     label: '2018',
                     method: 'update'
                 },
                 {
-                    args: [{'visible': [false, false, true, false,]},
-                           {'title': '2019 cluster'}],
+                    args: [{ 'visible': [false, false, true, false,] },
+                    { 'title': '2019 cluster' }],
                     label: '2019',
                     method: 'update'
                 },
                 {
-                    args: [{'visible': [false, false, false, true,]},
-                           {'title': '2020 cluster'}],
+                    args: [{ 'visible': [false, false, false, true,] },
+                    { 'title': '2020 cluster' }],
                     label: '2020',
                     method: 'update'
                 },
-    
+
             ],
             direction: 'left',
-            pad: {'r': 10, 't': 10},
+            pad: { 'r': 10, 't': 10 },
             showactive: true,
             type: 'buttons',
             xanchor: 'left',
             y: -0.2,
             yanchor: 'bottom'
         },
-    
+
     ]
 
 
@@ -250,64 +252,64 @@ function makePlot2(data) {
         updatemenus: updatemenus,
     }
 
-    Plotly.newPlot('secondPlot', data,layout, {displayModeBar: false});
+    Plotly.newPlot('secondPlot', data, layout, { displayModeBar: false });
 }
 
 function makePlot3(data) {
 
     clusters = make_clusters_by_word(data)
 
-    var data = [clusters[0],clusters[1],clusters[2],clusters[3],clusters[4],clusters[5]];
-    var updatemenus=[
+    var data = [clusters[0], clusters[1], clusters[2], clusters[3], clusters[4], clusters[5]];
+    var updatemenus = [
         {
             buttons: [
                 {
-                    args: [{'visible': [true, false, false, false, false, false]},
-                           {'title': 'All Blue Tweets'}],
+                    args: [{ 'visible': [true, false, false, false, false, false] },
+                    { 'title': 'All Blue Tweets' }],
                     label: 'All',
                     method: 'update'
                 },
                 {
-                    args: [{'visible': [false, true, false, false, false, false]},
-                           {'title': 'Blue Tweets containing fake'}],
+                    args: [{ 'visible': [false, true, false, false, false, false] },
+                    { 'title': 'Blue Tweets containing fake' }],
                     label: 'fake',
                     method: 'update'
                 },
                 {
-                    args: [{'visible': [false, false, true, false, false, false]},
-                           {'title': 'Blue Tweets containing radical'}],
+                    args: [{ 'visible': [false, false, true, false, false, false] },
+                    { 'title': 'Blue Tweets containing radical' }],
                     label: 'radical',
                     method: 'update'
                 },
                 {
-                    args: [{'visible': [false, false, false, true, false, false]},
-                           {'title': 'Blue Tweets containing sleepy'}],
+                    args: [{ 'visible': [false, false, false, true, false, false] },
+                    { 'title': 'Blue Tweets containing sleepy' }],
                     label: 'sleepy',
                     method: 'update'
                 },
                 {
-                    args: [{'visible': [false, false, false, false, true, false]},
-                           {'title': 'Blue Tweets containing corrupt'}],
+                    args: [{ 'visible': [false, false, false, false, true, false] },
+                    { 'title': 'Blue Tweets containing corrupt' }],
                     label: 'corrupt',
                     method: 'update'
                 },
                 {
-                    args: [{'visible': [false, false, false, false, false, true]},
-                           {'title': 'Blue Tweets containing Lamestream'}],
+                    args: [{ 'visible': [false, false, false, false, false, true] },
+                    { 'title': 'Blue Tweets containing Lamestream' }],
                     label: 'Lamestream',
                     method: 'update'
                 },
-    
+
             ],
             direction: 'left',
-            pad: {'r': 10, 't': 10},
+            pad: { 'r': 10, 't': 10 },
             showactive: true,
             type: 'buttons',
             xanchor: 'left',
             y: -0.2,
             yanchor: 'bottom'
         },
-    
+
     ]
 
 
@@ -325,134 +327,142 @@ function makePlot3(data) {
         updatemenus: updatemenus,
     }
 
-    Plotly.newPlot('thirdPlot', data,layout, {displayModeBar: false});
+    Plotly.newPlot('thirdPlot', data, layout, { displayModeBar: false });
 }
 
+var words, dates, months, steps, traces4 = []
+var currentword
 function makePlot4(data) {
+
 
     function unpack(data, key) {
         return data.map(function (data) { return data[key]; });
     }
 
-    var dates = [['2020-1-1', '2021-1-8'],['2020-1-1', '2020-1-31'], ['2020-2-1', '2020-2-29'], 
-    ['2020-3-1', '2020-3-31'], ['2020-4-1', '2020-4-30'], ['2020-5-1', '2020-5-31'],
-    ['2020-6-1', '2020-6-30'], ['2020-7-1', '2020-7-31'], ['2020-8-1', '2020-8-31'], 
-    ['2020-9-1', '2020-9-30'], ['2020-10-1', '2020-10-31'],['2020-11-1', '2020-11-30'], 
-    ['2020-12-1', '2021-1-8']]
-
-    var traces = [];
-    var words = ['fake','radical','sleepy','corrupt','Lamestream']
-    for (i=0; i<words.length; i++){
-        traces[i]={
-            type: "bar",
-            name: words[i],
-            x: unpack(data, 'date'),
-            y: unpack(data, words[i]),
-            line: { colorscale: 'Rainbow' }
-        }
+    words = ['fake', 'radical', 'sleepy', 'corrupt', 'Lamestream']
+    steps = []
+    colors = ["053c5e","1f7a8c","20a4f3","ce7b91","c1292e"]
+    traces4[0] = {
+        type: "bar",
+        name: [],
+        x: [],
+        y: [],
+        marker: {color:[]}
     }
-    console.log(traces)
-    var layout = { //layout information for the graph pretty standard 
 
-        barmode: 'stack',
-        width: 900,
-        font: {
-            size: 16,
-            family: "Heebo",
-            color: "black"
-        },
-        xaxis: {
-            fixedrange: false
+    for (i = 0; i < words.length; i++) {
+        traces4[0].name.push(words[i] + " cumulative tweets count")
+        traces4[0].x.push(words[i])
+        traces4[0].y.push(data[0][words[i]])
+        traces4[0].marker.color.push(colors[i])
+    }
+    var frames = []
+    var counter = 0
+
+    for (j = 0; j < 370; j+=7) {
+        var datum = data[j]
+        frames.push({
+            name: 'week '+(counter+1),
+            data: [{
+                x: [],
+                y: []
+            }],
            
-        },
-        yaxis: {
-            fixedrange: true
-        },
-        line: {
-            width: 1
-        },
-        hovermode: 'closest',
-
-        sliders: [{
-            pad: { t: 90 },
-            currentvalue: {
-                xanchor: 'right',
-                prefix: 'Month: ',
-                font: {
-                    color: '#888',
-                    size: 20
-                }
-            },
-
-            steps: [{
-                label: 'All',
-                method: 'relayout',
-                args: ['xaxis.range', dates[0]]
-            }, {
-                label: 'Jan',
-                method: 'relayout',
-                args: ['xaxis.range', dates[1]]
-            }, {
-                label: 'Feb',
-                method: 'relayout',
-                args: ['xaxis.range', dates[2]]
-            }, {
-                label: 'Mar',
-                method: 'relayout',
-                args: ['xaxis.range', dates[3]]
-            }, {
-                label: 'Apr',
-                method: 'relayout',
-                args: ['xaxis.range', dates[4]]
-            }, {
-                label: 'May',
-                method: 'relayout',
-                args: ['xaxis.range', dates[5]]
-            }, {
-                label: 'June',
-                method: 'relayout',
-                args: ['xaxis.range', dates[6]]
-            }, {
-                label: 'July',
-                method: 'relayout',
-                args: ['xaxis.range', dates[7]]
-            }, {
-                label: 'Aug',
-                method: 'relayout',
-                args: ['xaxis.range', dates[8]]
-            }, {
-                label: 'Sep',
-                method: 'relayout',
-                args: ['xaxis.range', dates[9]]
-            }, {
-                label: 'Oct',
-                method: 'relayout',
-                args: ['xaxis.range', dates[10]]
-            }, {
-                label: 'Nov',
-                method: 'relayout',
-                args: ['xaxis.range', dates[11]]
-            }, {
-                label: 'Dec/Jan',
-                method: 'relayout',
-                args: ['xaxis.range', dates[12]]
-            }]
-        }]
+        })
         
 
-    };
-
-
-
-    // Create the plot:
-    Plotly.newPlot('fourthPlot', {
-        data: traces,
-        layout: layout,
-        config: {
-            displayModeBar: false
+        for (i = 0; i < words.length; i++) {
+            frames[counter].data[0].y.push(datum[words[i]])
+            frames[counter].data[0].x.push(words[i])
         }
-    });
+        
+        steps.push(
+            {
+                label: ('week ' +(counter+1)),
+                method: 'animate',
+                args: [['week '+(counter+1)], {
+                    mode: 'immediate',
+                    transition: { duration: 300 },
+                    frame: { duration: 300 },
+                }]
+            })
+            counter++
+    }
+    
+
+var layout = { //layout information for the graph pretty standard 
+    barmode: "stack",
+    width: 900,
+    font: {
+        size: 16,
+        family: "Heebo",
+        color: "black"
+    },
+    xaxis: {
+
+    },
+    yaxis: {
+        range: [0,400]
+    },
+    line: {
+        width: 10
+    },
+    hovermode: 'closest',
+    
+    updatemenus: [{
+        x: 0,
+        y: 0,
+        yanchor: 'top',
+        xanchor: 'left',
+        showactive: false,
+        direction: 'left',
+        type: 'buttons',
+        pad: {t: 87, r: 10},
+        buttons: [{
+          method: 'animate',
+          args: [null, {
+            mode: 'immediate',
+            fromcurrent: true,
+            transition: {duration: 300},
+            frame: {duration: 300, redraw: false}
+          }],
+          label: 'Play'
+        }, {
+          method: 'animate',
+          args: [[null], {
+            mode: 'immediate',
+            transition: {duration: 0},
+            frame: {duration: 0, redraw: false}
+          }],
+          label: 'Pause'
+        }]
+      }],
+
+    sliders: [{
+        pad: { t: 40 },
+        currentvalue: {
+            xanchor: 'right',
+            font: {
+                color: '#888',
+                size: 20
+            }
+        },
+        steps: steps
+    }]
+
 }
+// Create the plot:
+Plotly.newPlot('fourthPlot', {
+    data: traces4,
+    layout: layout,
+    config: {
+        displayModeBar: false
+    },
+    frames: frames,
+});
+}
+
+
 
 
 Plotly.d3.csv(dataSource1, function (data) { makePlot1(data) })
